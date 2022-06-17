1aa26f033f63fec5426bf89f66c284652342d0fd
log1x0 <107345625+log1x0@users.noreply.github.com>
2022-06-17 16:29:33 +0200

    Version 1.7 (#3)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,13 +26,22 @@ public class Main {
         wc = new WebClient(BrowserVersion.FIREFOX);
         wc.getOptions().setJavaScriptEnabled(false);
         wc.getOptions().setCssEnabled(false);
-        wc.getCookieManager().addCookie(new Cookie("newheaven.nl", "uid", uid));
-        wc.getCookieManager().addCookie(new Cookie("newheaven.nl", "pass", pass));
+        String nhDomain = "newheaven.nl";
+        wc.getCookieManager().addCookie(new Cookie(nhDomain, "uid", uid));
+        wc.getCookieManager().addCookie(new Cookie(nhDomain, "pass", pass));
 
         final PopupMenu popupMenu = new PopupMenu();
-        final MenuItem csvItem = new MenuItem("Create csv file");
+        final Menu csvItem = new Menu("Create csv file");
         final MenuItem allItem = new MenuItem("Show last 10");
         final MenuItem exitItem = new MenuItem("Exit");
+        final MenuItem p1Item = new MenuItem("1 Page");
+        final MenuItem p2Item = new MenuItem("2 Pages");
+        final MenuItem p3Item = new MenuItem("3 Pages");
+        final MenuItem p4Item = new MenuItem("4 Pages");
+        csvItem.add(p1Item);
+        csvItem.add(p2Item);
+        csvItem.add(p3Item);
+        csvItem.add(p4Item);
         popupMenu.add(csvItem);
         popupMenu.add(allItem);
         popupMenu.add(exitItem);
@@ -46,13 +55,17 @@ public class Main {
         final SystemTray tray = SystemTray.getSystemTray();
         tray.add(trayIcon);
 
-        csvItem.addActionListener(e -> csvFile());
+        p1Item.addActionListener(e -> csvFile(1));
+        p2Item.addActionListener(e -> csvFile(2));
+        p3Item.addActionListener(e -> csvFile(3));
+        p4Item.addActionListener(e -> csvFile(4));
         allItem.addActionListener(e -> lasts());
         exitItem.addActionListener(e -> System.exit(0));
 
         try {
             HtmlPage page = wc.getPage(sbUrl);
-            page.getAnchorByHref("index.php?strWebValue=extra&strWebAction=shoutbox&page=1");
+            String sbLink = "index.php?strWebValue=extra&strWebAction=shoutbox&page=1";
+            page.getAnchorByHref(sbLink);
         } catch (Exception e) {
             System.out.println("Please enter the correct uid and pass next time.");
             System.exit(0);
@@ -62,54 +75,56 @@ public class Main {
         scheduleTimer();
     }
 
-    private synchronized void csvFile() {
+    private synchronized void csvFile(int pages) {
         try (WebClient wc2 = new WebClient(BrowserVersion.FIREFOX)) {
             wc2.getCookieManager().addCookie(wc.getCookieManager().getCookie("uid"));
             wc2.getCookieManager().addCookie(wc.getCookieManager().getCookie("pass"));
             wc2.getOptions().setJavaScriptEnabled(false);
             wc2.getOptions().setCssEnabled(false);
-            HtmlPage page = wc2.getPage("https://newheaven.nl/index.php?strWebValue=torrent&strWebAction=list");
-            List<DomElement> as = page.getByXPath("//table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td/a").stream().map(o -> (DomElement) o).toList();
-            Pattern pat = Pattern.compile("'([^']+)'");
             ArrayList<ItemCsv> rows = new ArrayList<>();
-            for (DomElement a : as) {
-                String name = a.asNormalizedText();
-                String mouse = a.getAttribute("onmouseover");
-                List<String> rs = pat.matcher(mouse).results().map(mr -> mr.group(1)).toList();
-                if (!rs.isEmpty()) {
-                    System.out.println("rs = " + rs);
-                    int comments = Integer.parseInt(rs.get(2));
-                    int seeder = Integer.parseInt(rs.get(3));
-                    int lecher = Integer.parseInt(rs.get(4));
-                    String size = rs.get(5);
-                    int snatched = Integer.parseInt(rs.get(6));
-                    String from = rs.get(7);
-                    String[] sizeSplit = size.split(" ");
-                    String first = sizeSplit[0].replace(",", "");
-                    double sizeNormal = switch (sizeSplit[1]) {
-                        case "MB" -> Double.parseDouble(first) * 1000;
-                        case "GB" -> Double.parseDouble(first) * 1000000;
-                        default -> Double.parseDouble(first);
-                    };
-                    rows.add(new ItemCsv(name, from, comments, seeder, lecher, snatched, sizeNormal));
+            for (int pageNumber = 1; pageNumber <= pages; pageNumber++) {
+                String csvPrefix = "https://newheaven.nl/index.php?strWebValue=torrent&strWebAction=list&page=";
+                HtmlPage page = wc2.getPage(csvPrefix + pageNumber);
+                List<DomElement> as = page.getByXPath("//table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td/a").stream().map(o -> (DomElement) o).toList();
+                Pattern pat = Pattern.compile("'([^']+)'");
+                for (DomElement a : as) {
+                    String name = a.asNormalizedText();
+                    String mouse = a.getAttribute("onmouseover");
+                    List<String> rs = pat.matcher(mouse).results().map(mr -> mr.group(1)).toList();
+                    if (!rs.isEmpty()) {
+                        System.out.println("rs = " + rs);
+                        int comments = Integer.parseInt(rs.get(2));
+                        int seeder = Integer.parseInt(rs.get(3));
+                        int lecher = Integer.parseInt(rs.get(4));
+                        String size = rs.get(5);
+                        int snatched = Integer.parseInt(rs.get(6));
+                        String from = rs.get(7);
+                        String[] sizeSplit = size.split(" ");
+                        String first = sizeSplit[0].replace(",", "");
+                        double sizeNormal = switch (sizeSplit[1]) {
+                            case "MB" -> Double.parseDouble(first) * 1000;
+                            case "GB" -> Double.parseDouble(first) * 1000000;
+                            default -> Double.parseDouble(first);
+                        };
+                        rows.add(new ItemCsv(name, from, comments, seeder, lecher, snatched, sizeNormal));
+                    }
                 }
             }
             double maxSeeder = rows.stream().map(ItemCsv::seeder).max(Integer::compare).orElse(-1);
             double maxLecher = rows.stream().map(ItemCsv::lecher).max(Integer::compare).orElse(-1);
             double maxSnatched = rows.stream().map(ItemCsv::snatched).max(Integer::compare).orElse(-1);
             double maxSize = rows.stream().map(ItemCsv::sizeNormal).max(Double::compare).orElse(-1.0);
-            double maxIndex = rows.size() - 1;
             String[] headers = {
                     "Index", "Name", "From", "Comments", "Seeder", "Lecher", "Snatched", "Size MB", "Score"
             };
             try (CSVPrinter p = new CSVPrinter(new FileWriter("nh-" + System.currentTimeMillis() + ".csv"), CSVFormat.Builder.create(CSVFormat.DEFAULT).setQuoteMode(QuoteMode.ALL).setHeader(headers).build())) {
                 for (int i = 0; i < rows.size(); i++) {
                     ItemCsv r = rows.get(i);
-                    double score = (r.seeder / maxSeeder + r.lecher / maxLecher + r.snatched / maxSnatched + r.sizeNormal / maxSize + i / maxIndex) / 5.0;
+                    double score = (r.seeder / maxSeeder + r.lecher / maxLecher + r.snatched / maxSnatched + r.sizeNormal / maxSize) / 4.0;
                     p.printRecord(i + 1, r.name, r.from, r.comments, r.seeder, r.lecher, r.snatched, (float) (r.sizeNormal / 1000.0), (float) score);
                 }
             }
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
